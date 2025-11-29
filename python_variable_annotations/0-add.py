73c326c5acf429fa91681f798f2561ea9dd6e818
JoacoZeba <joacozeba18@gmail.com>
2025-11-28 22:04:28 -0300

    .

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
+"""
+This module provides a simple addition function.
+
+Intended for basic arithmetic operations or as
+a utility function in larger program.
+"""
+
 
 def add(a: float, b: float) -> float:
+    """Return the sum of a and b.
+
+    Args:
+        a (float): first number to add.
+        b (float): The second number to add.
+
+    Returns:
+        The result of a + b.
+    """
     return a + b
