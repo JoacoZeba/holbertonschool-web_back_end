7f1e540bf441e644c406c4bd1d32a3f1cff08e1a
JoacoZeba <joacozeba18@gmail.com>
2025-11-28 22:07:17 -0300

    .

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""This module provides a simple concatenation function.
+
+Intended for basic string operations or as
+a utility function in larger programs.
+"""
+
+def concat(a: str, b: str) -> str:
+    """Return the concatenation of a and b.
+
+    Args:
+        a (str): The first string to concatenate.
+        b (str): The second string to concatenate.
+
+    Returns:
+        str: The result of a + b.
+    """
+    return a + b
