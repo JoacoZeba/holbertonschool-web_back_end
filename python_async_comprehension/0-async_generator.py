d6955e71caaa652064ebe156e80ead846433b396
JoacoZeba <joacozeba18@gmail.com>
2025-11-28 23:15:34 -0300

    .

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,7 +2,6 @@
 """
 This module defines an asynchronous generator that yields random numbers
 after asynchronously waiting for 1 second on each iteration.
-
 The `async_generator` coroutine loops 10 times, each time awaiting an
 asynchronous 1-second delay, then yielding a random float between 0 and 10.
 It can be used to simulate a stream of random data in an asynchronous context.
